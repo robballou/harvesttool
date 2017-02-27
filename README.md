e5271bb1a4d3aff53af82b9ff74ac4489f39fafc
Rob Ballou <rob@atendesigngroup.com>
2017-02-27 12:35:38 -0700

    New commands, started README

---
+++ README.md
@@ -0,0 +1,26 @@
+# HarvestTool
+
+Harvest CLI tool.
+
+## Usage
+
+```
+# output today's time
+harvesttool
+```
+
+## Install
+
+Tested in Python 3 but may work in Python 2:
+
+```
+pip install -r requirements.txt
+```
+
+## Commands
+
+### timesheet
+
+* `timesheet.today`: output today's time
+* `timesheet.billable`: output today's billable time
+* `timesheet.total`: output today's total time
--- harvesttool/command/timesheet.py
+++ harvesttool/command/timesheet.py
@@ -6,20 +6,45 @@ import subprocess
 import shutil
 import requests
 
+def get_sums(data):
+    day_sum = sum([entry['hours'] for entry in data['day_entries']])
+
+    project_ids = [int(entry['project_id']) for entry in data['day_entries']]
+    projects = {}
+    for project in data['projects']:
+        if int(project['id']) in project_ids:
+            projects[int(project['id'])] = project
+    billable_sum = sum([entry['hours'] for entry in data['day_entries'] if projects[int(entry['project_id'])]['billable']])
+    return (billable_sum, day_sum)
+
+class BillableCommand(Cmd):
+    cmd = 'billable'
+    formatter = 'output.stdout'
+
+    def run(self, args):
+        data = self.tool.get('/daily')
+
+        (billable_sum, day_sum) = get_sums(data)
+        return billable_sum
+
+class TotalCommand(Cmd):
+    cmd = 'total'
+    formatter = 'output.stdout'
+
+    def run(self, args):
+        data = self.tool.get('/daily')
+
+        (billable_sum, day_sum) = get_sums(data)
+        return day_sum
+
 class TodayCommand(Cmd):
     cmd = 'today'
     formatter = 'table.grid'
 
     def run(self, args):
         data = self.tool.get('/daily')
-        day_sum = sum([entry['hours'] for entry in data['day_entries']])
 
-        project_ids = [int(entry['project_id']) for entry in data['day_entries']]
-        projects = {}
-        for project in data['projects']:
-            if int(project['id']) in project_ids:
-                projects[int(project['id'])] = project
-        billable_sum = sum([entry['hours'] for entry in data['day_entries'] if projects[int(entry['project_id'])]['billable']])
+        (billable_sum, day_sum) = get_sums(data)
 
         result = OrderedDict()
         result['billable'] = billable_sum
